3b2d069bfd581fca58bfd16e897c89430a141866
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 02:02:48 +1000

    Update 5_Math.java

--- 5_Math.java
+++ 5_Math.java
@@ -1,1 +1,22 @@
-
+public class MathPrac {
+  public static void main(String[] args) {
+    // Math.max(x,y) method can be used to find the highest value of x and y
+    // output = 10
+    System.out.println(Math.max(5,10));
+  
+    // Math.min(x,y) method can be used to find the lowest value of x and y
+    // output = 5
+    System.out.println(Math.min(5,10));
+    
+    // Math.sqrt(x) method returns the squar root of x;
+    // output = 8.0
+    System.out.println(Math.sqrt(64));
+    
+    // Math.abs(x) method returns the absolute (positive) value of x
+    // output = 4.7
+    System.out.println(Math.abs(-4.7));
+  
+    // Math.random() returns a random number between 0 (inclusive) and 1 (exclusive)
+    
+  }
+}

a3e33f83b984741c5e276076569d962cde223d71
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 02:15:41 +1000

    Update 6_Booleans.java

--- 6_Booleans.java
+++ 6_Booleans.java
@@ -1,5 +1,23 @@
 public class Java_booleans {
   public static void main(String[] args) {
-  
+  /* a boolean type is declared with boolean keyword
+   * and can only take the values true or false
+  */
+    // output = true  false
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+    
+   /* Boolean Expression is a Java expression that returns Boolean value
+    * true or false
+    * can use a comparison operator
+    * such as the GREATHER THAN (>) operator to rind out if an expression is true
+   */
+    int x = 10, y = 9;
+    System.out.println(x > y); // output = true
+    
+    int x = 10;
+    System.out.println(x == 10); // output = true
   }
 }

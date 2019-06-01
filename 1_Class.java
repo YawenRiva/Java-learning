17c01b4a80de2652bf3dc09a2b4d93f89d4f6b95
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 00:27:00 +1000

    Create 1_Class.java

---
+++ 1_Class.java
@@ -0,0 +1,19 @@
+1. public class Myclass {}
+
+/*every line of code that runs in Java must be inside a CLASS.
+ *the class of the following code named class Myclass
+ *a class should always start with an UPPERCASE FIRST LETTER
+ *the name of JAVE FILE MUST MATCH THE CLASS NAME
+*/
+
+2. public static void main(String[] args) {}
+
+/*the main() method is required and it is in every java program
+ *any code INSIDE the main() methos will be EXECUTED
+*/
+
+public class Myclass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

d95fce0d9b1c9db21e2cfd06bbcfe9958a35fcd0
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 12:25:21 +1000

    Update 8_Switch_statement.java

--- 8_Switch_statement.java
+++ 8_Switch_statement.java
@@ -1,1 +1,64 @@
-
+/* java switch statements
+ * is used to select one of many code blocks to be excuted
+ * switch (expression) {
+ *    case x:
+ *      // code block
+ *      break;
+ *    case y:
+ *      // code block
+ *      break;
+ *    default:
+ *      // code block
+ * }
+ * the switch expression is evaluated once
+ * the value of the expression is compared with values of each case.
+ * if there is a match, the associated block of code is executed
+ * the break and default keywords are optional
+*/
+public class Switches {
+    public static void main(String[] args) {
+        // first switch case, output = Thursday
+        // if there is no break for each case
+        // the output would be: Thursday  Friday
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            
+          // Default including example
+          // output = This is the weekend
+           int day = 6;
+           switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("This is the weekend");
+            }
+    }
+}

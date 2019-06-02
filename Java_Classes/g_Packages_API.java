87757ffb5bc6985e3c153333460e17da6fab1a24
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-02 21:18:26 +1000

    Update g_Packages_API.java

--- Java_Classes/g_Packages_API.java
+++ Java_Classes/g_Packages_API.java
@@ -1,1 +1,27 @@
+/* Package in Java is used to group related classes.
+ * think of it as a folder in a file directory, avoid name conflict
+ * 1. Build-in Packages (packages from Java API)
+ * 2. User-defined Packages (create your own packages)
+*/
 
+// import a class
+// Scanner- used to get user input
+import java.util.Scanner;
+class Myclass {
+  Scanner myObj = new Scanner(System.in);
+  System.out.println("Enter username: ");
+  String userName = myObj.nextLine();
+  System.out.println("Username is: " + userName);
+}
+
+// user defined package
+package mypack;
+class MyPackageClass { 
+  public static void main(String[] args) { 
+    System.out.println("This is my package!"); 
+  } 
+}
+
+C:\Users\Your Name>javac MyPackageClass.java
+C:\Users\Your Name>javac -d . MyPackageClass.java
+C:\Users\Your Name>java mypack.MyPackageClass

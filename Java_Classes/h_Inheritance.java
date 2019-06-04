c00c9cfb8d549b847938945d0bcafc9206b6a22f
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-04 15:55:04 +1000

    Create h_Inheritance.java

---
+++ Java_Classes/h_Inheritance.java
@@ -0,0 +1,8 @@
+/* It is possible to inherit Attributes and Methods from one class to another
+ * 1. subclass(child) - the class that inherits from another class
+ * 2. superclass(parent) - the class being inherited from
+ * to nherit from a class, use the extends keyword.
+*/
+class Vehicle {
+
+}

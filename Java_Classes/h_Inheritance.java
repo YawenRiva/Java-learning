a73463322a349a148bd97d8bf7285f17cc418dcd
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-06-04 16:12:03 +1000

    Update h_Inheritance.java

--- Java_Classes/h_Inheritance.java
+++ Java_Classes/h_Inheritance.java
@@ -4,5 +4,31 @@
  * to nherit from a class, use the extends keyword.
 */
 class Vehicle {
+  protected String brand = "Ford";
+  // this is the Vehicle attribute
+  // if it is a private, the Car class cannot access it
+  // the protected modifier always keeps the parent-child relationship
+  
+  // because it is a parent/superclass it doesnt has to have a main()
+  public void honk() {
+    System.out.println("Tuut!");
+  }
+  // this honk() is a public method : what can the object do
+}
 
+class Car extends Vehicle {
+  /* as it is extends from the Vehicle, so it has:
+   * String brand = "Ford";
+   * public void honk()
+  */
+  private String modelName = "Mustang";
+  // this is a Car attribute
+  
+  public static void main(String[] args) {
+    // even it is the inherited class, the honk() is a public method
+    // we need to class new object newCar to call the public method
+    Car myCar = new Car();
+    myCar.honk();
+    System.out.println(myCar.brand + " " + myCar.modelName);
+  }
 }
